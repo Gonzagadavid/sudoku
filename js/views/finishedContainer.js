a70ea30bf9cb9984c402ca7b3c50b4767deacb6d
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-06-19 00:48:18 -0300

    iniciada funcao responsavel pela renderizacao do container do fim de jogo

--- js/views/finishedContainer.js
+++ js/views/finishedContainer.js
@@ -1,6 +1,14 @@
 import createHtmlElement from '../functions/createHtmlElement.js';
+import fetchBoard from '../functions/fetchBoard.js';
+import gameState from '../index.js';
 
 export default function finishedContainer() {
   const container = createHtmlElement('div', { className: 'finished-contaienr' });
   container.appendChild(createHtmlElement('h2', { innerHTML: 'Finished' }));
+  const text = `Parabéns! Você concluiu no level ${gameState.difficulty}.`;
+  container.appendChild(createHtmlElement('p', { innerHTML: text }));
+  const btnAttributes = { innerHTML: 'Joga novamente', className: 'btn-finished' };
+  const btn = createHtmlElement('button', btnAttributes);
+  const url = `https://sugoku.herokuapp.com/board?difficulty=${gameState.difficulty}`;
+  btn.addEventListener('click', () => fetchBoard(url, true));
 }
