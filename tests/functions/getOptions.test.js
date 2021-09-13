ec8740f0dd3fff21556ad3369068cca6b34fdf1d
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-09-12 23:36:38 -0300

    desenvolvido testes para a funcao getOptions

---
+++ tests/functions/getOptions.test.js
@@ -0,0 +1,58 @@
+import { expect, describe, it } from '@jest/globals';
+import functions from '../../js/functions';
+import boards from '../mock/boards';
+
+const { getOptions } = functions;
+const { board } = boards;
+
+describe('verifica a funcao getOptions', () => {
+  it('Ao passar o board e a posicao de um numero no primeiro quadrante retorna'
+  + ' um array com as opcoes corretas', () => {
+    expect(getOptions(1, 1, board)).toEqual([2, 4, 7]);
+  });
+
+  it('Ao passar o board e a posicao de um numero no segundo quadrante retorna'
+  + ' um array com as opcoes corretas', () => {
+    expect(getOptions(2, 4, board)).toEqual([3, 4]);
+  });
+
+  it('Ao passar o board e a posicao de um numero no terceiro quadrante retorna'
+  + ' um array com as opcoes corretas', () => {
+    expect(getOptions(1, 7, board)).toEqual([2, 3, 4]);
+  });
+
+  it('Ao passar o board e a posicao de um numero no quarto quadrante retorna'
+  + ' um array com as opcoes corretas', () => {
+    expect(getOptions(4, 1, board)).toEqual([2, 5]);
+  });
+
+  it('Ao passar o board e a posicao de um numero no quinto quadrante retorna'
+  + ' um array com as opcoes corretas', () => {
+    expect(getOptions(4, 4, board)).toEqual([5]);
+  });
+
+  it('Ao passar o board e a posicao de um numero no sexto quadrante retorna'
+  + ' um array com as opcoes corretas', () => {
+    expect(getOptions(4, 7, board)).toEqual([2, 5, 9]);
+  });
+
+  it('Ao passar o board e a posicao de um numero no setimo quadrante retorna'
+  + ' um array com as opcoes corretas', () => {
+    expect(getOptions(7, 1, board)).toEqual([2, 7, 8]);
+  });
+
+  it('Ao passar o board e a posicao de um numero no oitavo quadrante retorna'
+  + ' um array com as opcoes corretas', () => {
+    expect(getOptions(6, 4, board)).toEqual([3, 5]);
+  });
+
+  it('Ao passar o board e a posicao de um numero no nono quadrante retorna'
+  + ' um array com as opcoes corretas', () => {
+    expect(getOptions(7, 7, board)).toEqual([3]);
+  });
+
+  it('Ao passar o board e a posicao  ja preenchida um erro e lancado'
+  + ' um array com as opcoes corretas', () => {
+    expect(() => getOptions(1, 4, board)).toThrowError(/verificao de espaco preenchido/);
+  });
+});
