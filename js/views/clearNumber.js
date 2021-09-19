527d1ee1c1ca81f80f905607cc03f7999adf819e
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-09-18 23:44:11 -0300

    adicionado responsividade

--- js/views/clearNumber.js
+++ js/views/clearNumber.js
@@ -1,4 +1,5 @@
 export default function clearNumber(element, board) {
+  if (!element.innerHTML) return;
   const x = +element.id.replace(/(x=)|y=\d/g, '');
   const y = +element.id.replace(/(y=)|x=\d/g, '');
   board[x][y] = 0;
